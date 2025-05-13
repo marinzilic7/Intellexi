1004e82f040e0a37be437c0169cf07eb7e35dcbb
marinzilic7 <marinzilic507@gmail.com>
2025-05-13 16:06:36 +0200

    implementacija poruka o dohvatu i greski dohvacanja tecajnice

--- frontend/src/views/Details.tsx
+++ frontend/src/views/Details.tsx
@@ -17,14 +17,21 @@ const Details = () => {
   const [rate, setRate] = useState<Rate | null>(null);
   const [loading, setLoading] = useState(true);
 
+  const [success, setSuccess] = useState(false); 
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     const fetchRate = async () => {
       try {
         const res = await axios.get(`http://localhost:8080/rates/${id}`);
         setRate(res.data);
         console.log(res.data);
+        setSuccess(true);
+        setTimeout(() => setSuccess(false), 3000);
       } catch (err) {
         console.error("Greška:", err);
+        setError(true);
+        setTimeout(() => setError(false), 3000);
       } finally {
         setLoading(false);
       }
@@ -33,11 +40,28 @@ const Details = () => {
     fetchRate();
   }, [id]);
 
+  const formatDate = (isoDate: string) => {
+    const [year, month, day] = isoDate.split("-");
+    return `${day}.${month}.${year}`;
+  };
+
   if (loading) return <p>Učitavanje...</p>;
   if (!rate) return <p>Tečajnica nije pronađena.</p>;
 
   return (
     <div>
+        <h2 className="text-center">Detalji tečajnice</h2>
+        {success && (
+            <div className="alert alert-success w-25 position-absolute end-0 top-0 mt-5 me-5 text-center" role="alert">
+                No error
+            </div>
+            )}
+        {error && (
+            <div className="alert alert-danger" role="alert">
+                Došlo je do greške prilikom učitavanja tečajnice.
+            </div>
+        )}
+
       <table className="table  table-striped table-bordered">
         <thead>
           <tr>
@@ -51,7 +75,7 @@ const Details = () => {
         </thead>
         <tbody>
           <tr>
-            <td>{rate.datumPrimjene}</td>
+            <td>{formatDate(rate.datumPrimjene)}</td>
             <td>{rate.sifraValute}</td>
             <td>{rate.valuta}</td>
             <td>{rate.kupovni_tecaj}</td>
