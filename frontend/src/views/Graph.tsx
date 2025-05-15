a1a3149b418f50267e27704179562e37ffb2ebb7
marinzilic7 <marinzilic507@gmail.com>
2025-05-15 12:05:28 +0200

    instalacija recharta & testiranje sa testnim podacima

--- frontend/src/views/Graph.tsx
+++ frontend/src/views/Graph.tsx
@@ -1,8 +1,24 @@
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+const data = [
+  { datum: '2024-05-01', srednji_tecaj: 7.45 },
+  { datum: '2024-05-02', srednji_tecaj: 7.42 },
+  { datum: '2024-05-03', srednji_tecaj: 7.48 },
+];
+
+
+
 function Graph() {
-  return (
-    <div>
-      <h1 className="text-center mt-5 mb-5">Grafički prikaz</h1>
-    </div>
+ return (
+    <LineChart width={800} height={400} data={data}>
+      <CartesianGrid stroke="#ccc" />
+      <XAxis dataKey="datum" />
+      <YAxis domain={['dataMin', 'dataMax']} />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="srednji_tecaj" stroke="#8884d8" />
+    </LineChart>
   );
 }
 export default Graph;
